ba3777743928f66e29871026d1cbe210a7f991ad
Robert Kotz <robertkotz@macbookpro.lan>
2026-04-24 16:55:50 -1000

    not sure what this was about... is there a missing config for conform?

--- .config/nvim/lua/strangeloop/init.lua
+++ .config/nvim/lua/strangeloop/init.lua
@@ -23,7 +23,7 @@ require('strangeloop.strikethrough')
 require('strangeloop.luasnip')
 require('strangeloop.CopilotChat')
 require('strangeloop.mcphub')
-require('strangeloop.conform')
+--require('strangeloop.conform')
 require("luasnip.loaders.from_lua").lazy_load({paths = "~/.config/nvim/LuaSnip/"})
 
 local neoscroll = require('neoscroll')
